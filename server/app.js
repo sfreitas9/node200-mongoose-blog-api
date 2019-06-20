c6342970f3631466497bedd79374285d269f7f1a
Sherry Freitas <sherry.freitas@gmail.com>
2019-06-20 15:44:32 -0700

    adds MLab

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true});
+//in development, use:  'mongodb://localhost/my-blog'
+mongoose.connect('mongodb://heroku_bmpp196j:vngsalu4c09ssam6k8e5rptfmg@ds341557.mlab.com:41557/heroku_bmpp196j', { useMongoClient: true});  
 mongoose.Promise = Promise;
 
 app.use(bodyParser.json());
